a6626d6c2de62d96d46d20271698622a9ee9431b
lior.margalit <lior.margalit@hcl.com>
2017-12-31 18:17:31 +0200

    issue: try catch

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const browseUrl = 'http://render-tron.appspot.com/';
 const ENTER_A_URL_SELECTOR = 'input[type=url]'
 const TAKE_SCREENSHOT_SELECTOR = '#options > button:nth-child(1) > i'
 
-var countriesSuffix = [ 'es',
-                        'co.il',
-                        'com.ar',
-                        'cl',
-                        'pr',
+var countriesSuffix = [ //'es',
+                        //'co.il',
+                        //'com.ar',
+                        //'cl',
+                        //'pr',
                         'com.br',
                         'ca',
                         'mx',
@@ -21,7 +21,7 @@ async function rendertronTakeScreenshot() {
     for (let i=0 ; i < countriesSuffix.length; i++){
         for (let j=0; j < devices.length; j++){
             const browser = await puppeteer.launch({
-                headless: false,
+                headless: true,
                 gpu: false,
                 scrollbars: false,
                 args: ['--reduce-security-for-testing', '--deterministic-fetch','--disable-background-networking' ]
@@ -31,8 +31,13 @@ async function rendertronTakeScreenshot() {
         
             await page.emulate(devices[j]);
             await page.goto(browseUrl);
-        
-            await page.waitForSelector(ENTER_A_URL_SELECTOR,{timeout:1000});
+            
+            try {
+                await page.waitForSelector(ENTER_A_URL_SELECTOR,{timeout:5000});
+            } catch (error) {
+                continue;
+            }
+            
         
             enterUrlElement = await page.$(ENTER_A_URL_SELECTOR);
         
@@ -43,7 +48,7 @@ async function rendertronTakeScreenshot() {
             await takeScreenshotElement.click();
     
             try {
-                await page.waitForSelector('img',{timeout:5000});
+                await page.waitForSelector('img',{timeout:8000});
                 let screenShot = await page.$('img')
                 await page.waitFor(3000)
             } catch (error) {
