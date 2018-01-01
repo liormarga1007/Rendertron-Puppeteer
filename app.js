6d0eda427ae0b31d7160bca792c4e7fde279b379
lior.margalit <lior.margalit@hcl.com>
2018-01-01 11:35:15 +0200

    add countries

--- app.js
+++ app.js
@@ -4,36 +4,23 @@ const devices = require('puppeteer/DeviceDescriptors');
 const browseUrl = 'http://render-tron.appspot.com/';
 const ENTER_A_URL_SELECTOR = 'input[type=url]'
 const TAKE_SCREENSHOT_SELECTOR = '#options > button:nth-child(1) > i'
+const RENDER_SERIALIZE_SELECTOR = '#options > button:nth-child(2) > i'
+const RENDER_SERIALIZE_WITH_WEB_COMPONENTS_SELECTOR = '#options > button:nth-child(3) > i'
+
+const buttons=[ 
+                TAKE_SCREENSHOT_SELECTOR,
+                RENDER_SERIALIZE_SELECTOR,
+                RENDER_SERIALIZE_WITH_WEB_COMPONENTS_SELECTOR
+                ];
 
 var countriesSuffix = [ 
-                        /* 'es',
-                        'co.il',
-                        'com.ar',
-                        'cl',
-                        'pr',
-                        'com.br',
-                        'ca',
-                        'mx',
-                        'co.jp',
-                        'hk',
-                        'kr',
-                        'co.th',
-                        'com.au', */
-                        'co.nz',
-                        'ph',
-                        'vn',
-                        'co.id',
-                        'ch',
-                        'in',
-                        'ae',
-                        'ba',
-                        'ua',
-                        'le',
-                        'be',
-                        'by'
-
-
-                ];
+                        'com.ar','cl','pr','com.br',
+                        'ca','mx','com',
+                        'co.jp','hk','kr','co.th','com.au','co.nz','ph','vn','co.id','ch','in',
+                        'ae','ba','ua','le','be','by','es','it','ru','sk','pl','bg','gr','dk','pt','no','is','cz','hu','de','nl','fr','ro','lu','lv','fi',
+                        'com.eg','com.tr','co.il','com.kw','com.sa','com.jo',
+                        'si','se','ee','at','rs','com.tw','me'
+                        ];
 
 async function rendertronTakeScreenshot() {
     for (let i=0 ; i < countriesSuffix.length; i++){
@@ -68,15 +55,18 @@ async function rendertronTakeScreenshot() {
             const ENTER_A_URL = `https://www.youtube.${countriesSuffix[i]}/watch?v=kaH-nx6owmg`
             await enterUrlElement.type(ENTER_A_URL);
         
-            takeScreenshotElement = await page.$(TAKE_SCREENSHOT_SELECTOR);
-            await takeScreenshotElement.click();
-    
-            try {
-                await page.waitForSelector('img',{timeout:8000});
-                let screenShot = await page.$('img')
-                await page.waitFor(3000)
-            } catch (error) {
-                console.log('load more than 8 sec')
+            for (let k=0; k< buttons.length; k++){
+                await page.waitFor(buttons[k])
+                renderElement = await page.$(buttons[k]);
+                await renderElement.click();
+        
+                try {
+                    await page.waitForSelector('img',{timeout:8000});
+                    let screenShot = await page.$('img')
+                    await page.goBack();                    
+                } catch (error) {
+                    console.log('load more than 8 sec')
+                }
             }
             
             browser.close();
